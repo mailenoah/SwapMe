3119cc0abe2dc1a09ac260cf7503dcfcf5e43504
disegomaile1980 <u21477729@tuks.co.za>
2025-04-13 03:56:46 +0200

    Add project files.

---
+++ EXER 2a/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿
+namespace EXER_2a
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSwap = new System.Windows.Forms.Button();
+            this.txtbox2 = new System.Windows.Forms.TextBox();
+            this.txtbox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnSwap
+            // 
+            this.btnSwap.Location = new System.Drawing.Point(360, 201);
+            this.btnSwap.Name = "btnSwap";
+            this.btnSwap.Size = new System.Drawing.Size(99, 54);
+            this.btnSwap.TabIndex = 0;
+            this.btnSwap.Text = "SWAP";
+            this.btnSwap.UseVisualStyleBackColor = true;
+            this.btnSwap.Click += new System.EventHandler(this.btnSwap_Click);
+            // 
+            // txtbox2
+            // 
+            this.txtbox2.Location = new System.Drawing.Point(495, 100);
+            this.txtbox2.Name = "txtbox2";
+            this.txtbox2.Size = new System.Drawing.Size(120, 26);
+            this.txtbox2.TabIndex = 1;
+            // 
+            // txtbox1
+            // 
+            this.txtbox1.Location = new System.Drawing.Point(226, 100);
+            this.txtbox1.Name = "txtbox1";
+            this.txtbox1.Size = new System.Drawing.Size(111, 26);
+            this.txtbox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtbox1);
+            this.Controls.Add(this.txtbox2);
+            this.Controls.Add(this.btnSwap);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSwap;
+        private System.Windows.Forms.TextBox txtbox2;
+        private System.Windows.Forms.TextBox txtbox1;
+    }
+}
+
